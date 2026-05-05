b141d782720d4d5db260bfb94b1d096d8d802ff1
ItsTuki <rar56721@gmail.com>
2026-05-05 10:59:37 -0400

    Exception creacion

---
+++ msvc-pacientes/src/main/java/com/hospital/msvc_pacientes/exception/PacientesException.java
@@ -0,0 +1,7 @@
+package com.hospital.msvc_pacientes.exception;
+
+public class PacientesException extends RuntimeException {
+    public PacientesException(String message) {
+        super(message);
+    }
+}
